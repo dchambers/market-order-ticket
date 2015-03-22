24cf221f512dd08585c7c78ece8b182262949399
dchambers <dominic.chambers@gmail.com>
2015-03-22 21:50:29 +0000

    Allow the Flux constructor to be injected.

--- src/MarketOrderTicket.js
+++ src/MarketOrderTicket.js
@@ -1,6 +1,22 @@
 'use strict';
 
 /*
+Global Stores:
+	Orders Store: list of orders, including pending orders (from the site / local storage), active orders (from the
+exchange), and completed orders (from Ethereum).
+	Position Store: list of active positions (derived from the order store).
+
+Ticket Stores:
+	Order Store: a single pending or active order from the Orders Store
+	OrderBook Store: created on an as needed basis by tickets.
+	OrderQuote Store: returns indicative long and short prices given an instrument and an amount.
+
+---
+
+The Flux for a particular ticket will contain an Order Store containing a single order from the Order X Store.
+
+---
+
 order {
 	instrument: <instrument>,
 	type: long|short,
@@ -31,27 +47,37 @@ instrument (prop)
 amount (user input state)
 longPrice (state derived from order book)
 shortPrice (state derived from order book)
-state: awaitingMarketData -> awaitingOrder -> awaitingOrderAck -> (orderAckConfirmed) | (orderAckDenied -> awaitingOrder)
+state: awaitingMarketData -> awaitingOrder -> awaitingOrderAck -> (orderAckConfirmed -> orderFilled | orderFailed) | (orderAckDenied -> awaitingOrder)
 (component managed state)
 amountVolumeHistogram (state derived from order book)
 */
 
 import React from 'react';
 import Immutable from 'immutable';
-import Flux from './Flux';
 import InnerComponent from './InnerComponent';
+
+const awaitingMarketData = 1;
+const awaitingOrder = 2;
+const awaitingOrderAck = 3;
+const orderAckConfirmed = 4;
+const orderAckDenied = 5;
+const orderFilled = 6;
+const orderFailed = 7;
 
 export default class MarketOrderTicket extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = new Immutable.Map({
-			amount: props.initialAmount
+			amount: props.initialAmount,
+			state: awaitingMarketData,
+			longPrice: null,
+			shortPrice: null
 		});
 	}
 
 	getChildContext() {
 		return {
-			flux: new Flux(),
+			flux: this.props.flux,
 			componentName: 'MarketOrderTicket'
 		};
 	}
@@ -75,6 +101,7 @@ export default class MarketOrderTicket extends React.Component {
 }
 
 MarketOrderTicket.propTypes = {
+	flux: React.PropTypes.any.isRequired,
 	instrument: React.PropTypes.string.isRequired,
 	initialAmount: React.PropTypes.number.isRequired
 };
